fa8e2f0bbb1d39972cf931104b056340cfbaf68c
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-02-16 16:27:16 -0700

    added an id to the allTodoList object and made the delete button use that ID to delete an item

--- script/main.js
+++ script/main.js
@@ -11,19 +11,21 @@
     // }
 let allTodoList = [];
 
-function addNewTodo(todoListName) {
+function onPageLoad() {
+    getNewTodo();
+    displayTodoList();
+    handleDeleteTodo();
+}
+
+function addNewTodo(todoListName, todoListId) {
     allTodoList.push({
         todoListName,
+        todoListId
     })
 
     displayTodoList()
 }
 
-function onPageLoad() {
-    getNewTodo();
-    displayTodoList();
-
-}
 
 function getNewTodo() {
     const buttonElement = document.querySelector(".newTodo");
@@ -32,7 +34,7 @@ function getNewTodo() {
         if (event.key === 'Enter') {
             // event.preventDefault()
             const todoListName = document.querySelector('.newTodo').value
-            addNewTodo(todoListName)
+            addNewTodo(todoListName, randomString(5))
             //clear input box
             clearInputBox(buttonElement)
             displayTodoTittle()
@@ -41,35 +43,44 @@ function getNewTodo() {
     
 }
 
-function handleDeleteTodo(){
-    const deleteElement = document.querySelector('');
+function handleDeleteTodo(parentID){
+    // const deleteElement = document.getElementById(parentID);
+    // const indexOfItem = (element) => element === parentID
+    // const index = allTodoList.map(e => e.name).indexOf(parentID);
+    const index = allTodoList.findIndex(item => item.todoListId === parentID);
+    deleteTodo(index)
+    displayTodoList()
+    // const arrayItemNum = deleteElement.parentElement.id
+    // deleteElement.addEventListener("onClick", deleteTodo(arrayItemNum))
 }
 
 function editTodo(newListName) {
     allTodoList.todoListName = newListName
 }
 
-function deleteTodo(itemNum) {
-    allTodoList.splice(itemNum, 1)
+function deleteTodo(idexNum) {
+    allTodoList.splice(idexNum, 1)
 }
 
 function displayTodoList() {
     const displayLocation = document.querySelector('.displayTodoList');
     let newInnerHTML = '';
     allTodoList.forEach(item => {
-        const { todoListName } = item
+        const { todoListName , todoListId } = item
         newInnerHTML += `
             <div class="displayTodoItem">
                 <div>${todoListName}</div>
-                <div>
-                    <img id="" src="images/icons8-pencil-24.png" alt="">
-                    <img id="" src="images/icons8-delete-trash-24.png" alt="">
+                <div id="${todoListId}">
+                    <img class="edit icon" onclick="" src="images/icons8-pencil-24.png" alt="">
+                    <img class="delete icon" onclick="handleDeleteTodo(this.parentElement.id)" src="images/icons8-delete-trash-24.png" alt="">
                 </div>
             </div>
         `
     });
     displayLocation.innerHTML = newInnerHTML
 }
+
+
 
 function displayTodoTittle() {
     const displayTodo = document.querySelector('.displayTodo')
@@ -79,6 +90,20 @@ function displayTodoTittle() {
 
 function clearInputBox(element) {
     element.value = '';
+}
+
+function randomString(length) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+
+    if (! length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+
+    var str = '';
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
 }
 
 window.onload = onPageLoad;
